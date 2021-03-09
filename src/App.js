dceba1478b7d1901269e839072c99da1a3c63223
feoNikpl <feo.pl@mail.ru>
2021-03-10 01:24:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Switch, Route,  BrowserRouter} from 'react-router-dom'
+import Content from "./components/content"
+import Author from "./components/author"
+import Authorization from "./components/authorization"
+import Header from './components/header'
+import Footer from './components/footer'
+import ContentTable from './components/contentTable'
+import './components/css/footer.css';
+import './components/css/header.css';
+import './components/css/Main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <main>
+          <div class="container">
+            <div class="content">
+              <Switch>
+                <Route path="/authorization"><Authorization/></Route>
+                <Route path="/content"><ContentTable/></Route>
+                <Route path="/"><Author/><br/><Content/></Route>
+              </Switch>
+            </div>
+          </div>
+        </main>
+        <Footer/>
+      </div>
+    </ BrowserRouter>
   );
 }
 
